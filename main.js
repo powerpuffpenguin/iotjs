17a06dfcaba3b955eaebffea1f29fa12d4c7ccff
zuiwuchang <zuiwuchang@gmail.com>
2023-06-27 11:24:41 +0800

    rm complate

--- main.js
+++ main.js
@@ -1,54 +1,54 @@
 // if ( iotjs.arch === "csky") {
 //     iotjs.nameserver("192.168.251.1:53")
 // }
-// var net = require("iotjs/net")
-// var WebsocketConn = net.WebsocketConn
-// try {
-//     WebsocketConn.connect(
-//         "wss://192.168.251.50:9443/abc",
-//         {
-//             timeout: 1000,
-//             insecure: true,
-//         }, function (c, e) {
-//             if (c) {
-//                 console.log("connect success", c)
-//                 c.debug = true
-//                 c.onError = function (e) {
-//                     if (e instanceof Error) {
-//                         console.log("js onError", e.toString())
-//                     } else {
-//                         console.log("js onError", e)
-//                     }
-//                 }
-//                 c.onClose = function () {
-//                     console.log("js onClose")
-//                 }
-//                 // c.onReadable = function () {
-//                 //     console.log("js onReadable")
-//                 // }
-//                 var i = 0
-//                 c.onMessage = function (data) {
-//                     if (typeof data == "string") {
-//                         console.log("js onMessage string", data.length)
-//                         console.log(data)
-//                     } else {
-//                         console.log("js onMessage Uint8Array", data.length)
-//                         console.log(data)
-//                     }
-//                     if (i < 100) {
-//                         c.trySend("this is message " + i)
-//                         i++
-//                     } else {
-//                         c.close()
-//                     }
-//                 }
-//             } else {
-//                 console.log("connect err:", e.toString())
-//             }
-//         })
-// } catch (e) {
-//     console.log('-------', e.toString())
-// }
+var net = require("iotjs/net")
+var WebsocketConn = net.WebsocketConn
+try {
+    WebsocketConn.connect(
+        "wss://192.168.251.50:9443/abc",
+        {
+            timeout: 1000,
+            insecure: true,
+        }, function (c, e) {
+            if (c) {
+                console.log("connect success", c)
+                c.debug = true
+                c.onError = function (e) {
+                    if (e instanceof Error) {
+                        console.log("js onError", e.toString())
+                    } else {
+                        console.log("js onError", e)
+                    }
+                }
+                c.onClose = function () {
+                    console.log("js onClose")
+                }
+                // c.onReadable = function () {
+                //     console.log("js onReadable")
+                // }
+                var i = 0
+                c.onMessage = function (data) {
+                    if (typeof data == "string") {
+                        console.log("js onMessage string", data.length)
+                        console.log(data)
+                    } else {
+                        console.log("js onMessage Uint8Array", data.length)
+                        console.log(data)
+                    }
+                    if (i < 100) {
+                        c.trySend("this is message " + i)
+                        i++
+                    } else {
+                        c.close()
+                    }
+                }
+            } else {
+                console.log("connect err:", e.toString())
+            }
+        })
+} catch (e) {
+    console.log('-------', e.toString())
+}
 // try {
 //     var mtd = require("iotjs/mtd")
 //     console.log(mtd)
@@ -69,12 +69,3 @@
 //     console.log('err', e.toString())
 // }
 
-var sha1 = require("iotjs/crypto/sha1")
-console.log(sha1.sum('123').length, sha1)
-
-setTimeout(function () {
-    console.log("-------")
-    setTimeout(function () {
-
-    }, 1000 );
-}, 1000);
